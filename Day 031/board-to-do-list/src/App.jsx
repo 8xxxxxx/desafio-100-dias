55fe0053df7406e44185f62d1510d04145c934b7
8xxxxxx <826.xxxxx+ghpublic@gmail.com>
2024-10-22 17:54:49 -0300

    Day 40

--- Day 031/board-to-do-list/src/App.jsx
+++ Day 031/board-to-do-list/src/App.jsx
@@ -1,21 +1,25 @@
 import { React, useState } from 'react';
 import Input from './components/input';
+import Board from './components/board';
 
 function App() {
   const [taskList, setTaskList] = useState([]);
 
   console.log(taskList);
   return (
-    <div className='flex flex-col items-center justify-center py-8 gap-4'>
-      <h1>Quadro de Tarefas</h1>
-        <Input taskList={taskList} setTaskList={setTaskList}/>
+    <>
+      <div className='flex flex-col items-center justify-center py-8 gap-4'>
+        <h1>Quadro de Tarefas</h1>
+          <Input taskList={taskList} setTaskList={setTaskList}/>
+        </div>
+        <div>
+          {taskList.map((task, index) => (
+            <Board key={index} task={task}/>
+          ))}
 
-      <div>
-        {taskList.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
-      </div>
-    </div>
+        </div>
+
+    </>
   )
 }
 
---
+++ Day 031/board-to-do-list/src/components/board.jsx
@@ -0,0 +1,14 @@
+import { useEffect, React } from 'react';
+
+const Board = ({ task }) => {
+    return (
+        <>
+            <div>
+                <p>{task}</p>
+            </div>
+        
+        </>
+    )
+}
+
+export default Board;
