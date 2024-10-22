e2711d380c68a6666cac92119f0e550e3a1ba4a2
8xxxxxx <826.xxxxx+ghpublic@gmail.com>
2024-10-22 17:27:05 -0300

    Day 39

--- Day 031/board-to-do-list/src/App.jsx
+++ Day 031/board-to-do-list/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import { React, useState } from 'react';
 import Input from './components/input';
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  console.log(taskList);
   return (
-    <>
+    <div className='flex flex-col items-center justify-center py-8 gap-4'>
       <h1>Quadro de Tarefas</h1>
-      <Input/>
-    </>
+        <Input taskList={taskList} setTaskList={setTaskList}/>
+
+      <div>
+        {taskList.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- Day 031/board-to-do-list/src/components/input.jsx
+++ Day 031/board-to-do-list/src/components/input.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { useState } from 'react';
 
-const Input = () => {
+const Input = (taskList, setTaskList) => {
     const [input, setInput] = useState("");
 
-    console.log(input);
+    const handleAddTask = (e) => {
+        e.preventDefault();
+        setTaskList([...taskList, input]);
+        setInput("");
+    }
     
     return (
         <>
-            <form>
+            <form className="flex flex-row items-center gap-3">
                 <input type="text"
-                    className="border rounded 2px"
+                    className="border rounded-1g py-1.5 px-2.5 text-1g"
                     placeholder="Adicione uma tarefa"
                     value={input}
                     onChange={(e) => setInput(e.target.value)}/>
-                <button>Adicionar Tarefa</button>
+                <button onClick={handleAddTask}
+                        className="bg-violet-400 text-white py-2 px-3.5 rounded-1g
+                                    font-semibold hover:opacity-70">Adicionar Tarefa</button>
             </form>
         </>
     )
