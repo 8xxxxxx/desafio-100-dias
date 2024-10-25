c47f9d78a40064fb769be650fc9538c7817767b8
8xxxxxx <826.xxxxx+ghpublic@gmail.com>
2024-10-24 23:02:44 -0300

    Day 46

--- Day 046/routes/index.js
+++ Day 046/routes/index.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req,res,next) => {
+    const data = {
+        name: "Home"
+    };
+    //res.send("This is my first rout with node + express");
+    res.render('index', data)
+});
+
+module.exports = router;
