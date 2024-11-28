f1e26f1ff27d22770b0bae5be8ba306815f9e1eb
8xxxxxx <826.xxxxx+ghpublic@gmail.com>
2024-11-27 23:32:20 -0300

    Days 67, 68, 69, 70, 71, 72, 73, 74, 75 and 76

--- Day 061/myblog/blog/views.py
+++ Day 061/myblog/blog/views.py
@@ -4,11 +4,11 @@ from .forms import PostForm
 
 def home(request):
     posts = Post.objects.all()
-    return render(request, 'blog/home.html', {'posts': posts})
+    return render(request, 'blog/home.html', { 'posts': posts })
 
 def post_detail(request, id):
     post = get_object_or_404 (Post, id=id)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post_detail.html', { 'post': post })
 
 def new_post(request):
     if request.method == 'POST':
@@ -18,4 +18,22 @@ def new_post(request):
             return redirect('home')
     else:
         form = PostForm()
-        return render(request, 'blog/new_post.html', {'form': form})
+        return render(request, 'blog/new_post.html', { 'form': form })
+
+def edit_post(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', id=post.id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/edit_post.html', { 'form': form })
+
+def delete_post(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('home')
+    return render(request, 'blog/delete_post.html', { 'post': post })
