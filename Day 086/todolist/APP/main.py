9fd2f001730aa671ecb1b87fb6bededc76be6898
8xxxxxx <826.xxxxx+ghpublic@gmail.com>
2024-12-02 15:05:58 -0300

    refactor: add templates folder, index file and some importations

--- Day 086/todolist/APP/main.py
+++ Day 086/todolist/APP/main.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 # Initialize the Flask application
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 # Define the home route
 @app.route('/')
-def home():
-    return "Welcome to the To-Do List app!"
+def index():
+    return render_template("index.html")
 
 # Run the application
 if __name__ == '__main__':
